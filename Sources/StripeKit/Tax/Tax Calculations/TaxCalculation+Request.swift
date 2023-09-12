d1a71dde1928bbf1bbd88a42ec7decb3ef7b6ba3
Ravil Khusainov <ravil@partof.team>
2023-09-12 11:00:54 +0400

    Inits added to TaxCalculation structs

--- Sources/StripeKit/Tax/Tax Calculations/TaxCalculation+Request.swift
+++ Sources/StripeKit/Tax/Tax Calculations/TaxCalculation+Request.swift
@@ -7,11 +7,22 @@ extension TaxCalculation {
         let currency: Currency
         let lineItems: [LineItem]
         let customerDetails: CustomerDetails
+        
+        public init(currency: Currency, lineItems: [LineItem], customerDetails: CustomerDetails) {
+            self.currency = currency
+            self.lineItems = lineItems
+            self.customerDetails = customerDetails
+        }
     }
     
     public struct LineItem: Encodable {
         public let amount: Int
         public let reference: String
+        
+        public init(amount: Int, reference: String) {
+            self.amount = amount
+            self.reference = reference
+        }
     }
     
     public struct CustomerDetails: Encodable {
@@ -20,6 +31,11 @@ extension TaxCalculation {
         public struct Address: Encodable {
             public let country: String
             public let postalCode: String
+            
+            public init(country: String, postalCode: String) {
+                self.country = country
+                self.postalCode = postalCode
+            }
         }
     }
 }
